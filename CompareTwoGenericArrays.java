40e8c9263b995d349ed9b3cf0726822d3d5fa1ec
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-22 20:28:01 +0100

    If you have two 'T' or 'V', don't write <T,T>, just write <T>

---
+++ CompareTwoGenericArrays.java
@@ -0,0 +1,27 @@
+public class CompareTwoGenericArrays<T> {
+    /* Write a Java program to create a generic method that takes two 
+    arrays of the same type and checks if they have the same elements 
+    in the same order. */
+
+    private T[] array1;
+    private T[] array2;
+
+    public CompareTwoGenericArrays(T[] array1, T[] array2){
+        this.array1 = array1;
+        this.array2 = array2;
+    }
+
+    public boolean compare(){
+        if (array1.length != array2.length){
+            return false;
+        }
+        for (int i = 0; i < array1.length; i++){
+            if (!array1[i].equals(array2[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+// Duplicate parameters (if there are two x T, then just do <T>)
