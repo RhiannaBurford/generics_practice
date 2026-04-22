864e0df8f3770af16825a01c4c7ed5d96dafebd1
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-22 19:33:58 +0100

    In the constructor you don't need <K, V>

---
+++ OrderedPair.java
@@ -0,0 +1,10 @@
+public class OrderedPair<K,V> {
+
+    private K key;
+    private V value;
+
+    public OrderedPair(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+}
